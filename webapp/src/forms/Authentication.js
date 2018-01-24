d4f03002fc9737a87a4da73fb38816d9c4f20c9e
sendevman <devman.sd@gmail.com>
2018-01-24 15:03:42 -0400

    first commit

---
+++ webapp/src/forms/Authentication.js
@@ -0,0 +1,76 @@
+/**
+ * Created by fran on 1/24/18.
+ */
+import React, {Component} from "react";
+import {Button, FormControl, FormGroup, Label, Row} from "react-bootstrap";
+import "./Authentication.css";
+import logo from './logo.svg';
+
+
+export default  class Authentication extends Component {
+    constructor(props) {
+        super(props);
+
+        this.handleUsernameChange = this.handleUsernameChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+
+        this.state = {
+            username: '',
+            password: ''
+        };
+    }
+
+    getValidationState(evalObj) {
+        const length = evalObj.length;
+        if (length > 10) return 'success';
+        else if (length > 5) return 'warning';
+        else if (length > 0) return 'error';
+        return null;
+    }
+
+    handleUsernameChange(e) {
+        this.setState({username: e.target.value});
+    }
+
+    handlePasswordChange(e) {
+        this.setState({password: e.target.value});
+    }
+
+
+    render() {
+        return (
+            <form className="login-block">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <h1 className="title"><Label bsStyle="primary">Matricula en Linea</Label></h1>
+                <FormGroup
+                    controlId="username"
+                    validationState={this.getValidationState(this.state.username)}
+                >
+                    <FormControl
+                        type="text"
+                        value={this.state.username}
+                        placeholder="usuario"
+                        onChange={this.handleUsernameChange}
+                    />
+                    <FormControl.Feedback />
+                </FormGroup>
+                <FormGroup controlId="password" validationState={this.getValidationState(this.state.password)}>
+                    <FormControl
+                        type="password"
+                        value={this.state.password}
+                        placeholder="contraseña"
+                        onChange={this.handlePasswordChange}
+                    />
+                    <FormControl.Feedback />
+                </FormGroup>
+                <Row>
+                    <Button className="login-button" bsStyle="primary" onClick={this.login}>Entrar</Button>
+                </Row>
+            </form>);
+    }
+
+    login = ()=>{
+        this.props.history.push("/Home")
+    }
+
+}
---
+++ webapp/src/forms/Home.js
@@ -0,0 +1,14 @@
+/**
+ * Created by fran on 1/24/18.
+ */
+import React, {Component} from "react";
+import {Row} from "react-bootstrap";
+
+export default class Home extends Component {
+
+    render() {
+        return (<Row>
+            <h1>Welcome to MEL....</h1>
+        </Row>);
+    }
+}
