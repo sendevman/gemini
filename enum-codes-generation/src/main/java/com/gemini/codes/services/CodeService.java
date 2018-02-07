cd1c8bdc9ba293182fe5717af799ecdccb137641
nativelocation <bluesky6460@mail.com>
2018-02-06 23:10:58 -0400

    added new java module that manage the REST service added new java module to create the javascript data pulled the data from SIE some minor UI changes

---
+++ enum-codes-generation/src/main/java/com/gemini/codes/services/CodeService.java
@@ -0,0 +1,35 @@
+package com.gemini.codes.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.SingleColumnRowMapper;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: fran
+ * Date: 1/29/18
+ * Time: 2:53 PM
+ */
+@Service
+public class CodeService extends JdbcDaoSupport {
+    @Autowired
+    DataSource dataSource;
+
+    @PostConstruct
+    void init() {
+        this.setDataSource(dataSource);
+    }
+
+    public List<String> getCodesForTable(String enumTable) {
+        return getJdbcTemplate().query(
+                String.format("SELECT " +
+                        "'{ name:  '''|| NAME || ''', description: '''|| DESCRIPTION||''', value: '''|| VALUE || ''', orderBy: '''|| ORDER_DEFAULT||'''}' as JSON_OBJECT " +
+                        "FROM %s ORDER BY ORDER_DEFAULT ", enumTable), new SingleColumnRowMapper<String>());
+    }
+
+}
---
+++ portal/webapp/src/forms/wizard/TransportationInfo.js
@@ -0,0 +1,73 @@
+/**
+ * Created by fran on 1/30/18.
+ */
+import React, {Component} from "react";
+import CodeSelect from "../../components/CodeSelect";
+
+export default class TransportationInfo extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {form: {}};
+    }
+
+    render() {
+        let form = this.state.form;
+        return (<form>
+            <div className="row">
+                <div className="col-md-6">
+
+                    <div className="form-group">
+                        <label htmlFor="transportationType">Tipo de transportaci&oacute;n</label>
+                        <CodeSelect id="transportationType"
+                                    onChange={(e) => {
+                                        this.setState({
+                                            form: {
+                                                transportationType: e.target.value
+                                            }
+                                        });
+                                    }}
+                                    codeType="transportationTypes"
+                                    placeholder="Seleccione su tipo de transaportacion"/>
+                    </div>
+                </div>
+                <div className="col-md-6">
+                    <div className="form-group">
+                        <label>Transportaci&oacute;n del Estudiante</label>
+                        <br/>
+                        <label className="radio-inline">
+                            <input type="radio" name="studentTransportation" id="transportationNeed"
+                                   checked={form.transportationType && form.transportationType !== 'PRVD'} disabled/>Necesita
+                            Transportacion
+                        </label>
+                        <label className="radio-inline">
+                            <input type="radio" name="studentTransportation" id="haveTransportation"
+                                   checked={form.transportationType && form.transportationType === 'PRVD'}
+                                   disabled/> Posee
+                            Transportacion
+                        </label>
+                    </div>
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-md-6">
+                    <div className="form-group">
+                        <label htmlFor="transportationDistance">Distancia</label>
+                        <input type="text" className="form-control" id="transportationDistance"
+                               placeholder="Distancia"/>
+                    </div>
+                </div>
+                <div className="col-md-6">
+                    <div className="form-group">
+                        <label>Necesidad del Estudiante</label>
+                        <br/>
+                        <label className="checkbox-inline">
+                            <input type="checkbox" id="studentSpecialNeed"/> Requiere transportacion Especial
+                        </label>
+                    </div>
+                </div>
+            </div>
+        </form>);
+    }
+}
